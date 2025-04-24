0441206a7b30dcf9f3597b54958856dfa2966910
Philipp Härtel <haertel.philipp@gmail.com>
2025-04-24 13:50:03 +0200

    add fig fcns to save output

--- dunkelflaute/visualize.py
+++ dunkelflaute/visualize.py
@@ -1,4 +1,31 @@
 from matplotlib import pyplot as plt
+import os
+
+
+def create_new_figure():
+    height = 13.8  # cm, for ppt
+    width = 31.2  # cm, for ppt
+
+    fig = plt.figure(figsize=(width / 2.54, height / 2.54), dpi=300)
+    plt.rcParams["font.family"] = "Linux Biolinum"
+    plt.rcParams["font.size"] = 10
+
+    return fig
+
+
+def save_figure(fig, filename):
+    plot_dir = "plots"
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+
+    filename = os.path.join(plot_dir, filename)
+    fig.savefig(
+        filename,
+        bbox_inches="tight",
+        pad_inches=0.1,
+        transparent=True,
+    )
+    plt.close(fig)
 
 
 def plot_dunkelflaute_events(
@@ -7,7 +34,7 @@ def plot_dunkelflaute_events(
     no_years = len(df_total.index.year.unique())
 
     for cap_mix in cap_mix_range:
-        plt.figure(figsize=(10, 6))
+        fig = create_new_figure()
         for threshold in thresholds:
             dunkelflaute_counts = []
             for period_len in period_lenghts:
@@ -36,9 +63,10 @@ def plot_dunkelflaute_events(
         plt.ylabel("Number of observed Dunkelflaute events (# of periods per year)")
         plt.legend()
         plt.grid()
+        save_figure(fig, f"dunkelflaute_events_{cap_mix}.svg")
 
     # Do the same in a single plot, putting all capacity mix ratios side by side in subplots horizontally
-    plt.figure(figsize=(15, 10))
+    fig = create_new_figure()
     for i, cap_mix in enumerate(cap_mix_range):
         y_max = 0
         plt.subplot(1, len(cap_mix_range), i + 1)
@@ -78,11 +106,11 @@ def plot_dunkelflaute_events(
         plt.ylim(0, y_max * 1.1)
 
     plt.tight_layout()
-    plt.show()
+    save_figure(fig, "dunkelflaute_events.svg")
 
 
 def plot_period_ts_data(results, df_total, cap_mix, threshold, period_len):
-    plt.figure(figsize=(10, 6))
+    fig = create_new_figure()
     for start, end in results[threshold][period_len][
         f"w{cap_mix:2.2f}_s{1-cap_mix:2.2f}"
     ]:
@@ -100,4 +128,25 @@ def plot_period_ts_data(results, df_total, cap_mix, threshold, period_len):
     plt.legend()
     plt.grid()
     plt.tight_layout()
-    plt.show()
+    save_figure(fig, "period_ts_data.svg")
+
+    # Save figure for each period
+    for i, (start, end) in enumerate(
+        results[threshold][period_len][f"w{cap_mix:2.2f}_s{1-cap_mix:2.2f}"]
+    ):
+        fig = create_new_figure()
+        plt.axvspan(start, end, color="red", alpha=0.3)
+        plt.plot(
+            df_total.index,
+            df_total[f"w{cap_mix:2.2f}_s{1-cap_mix:2.2f}"],
+            label="Total Production",
+        )
+        plt.title(
+            f"Dunkelflaute Event {i+1} for Wind: {cap_mix} Solar: {1-cap_mix}, Threshold: {threshold}, Min. Period Length: {int(period_len/24)} days"
+        )
+        plt.xlabel("Date")
+        plt.ylabel("Production")
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        save_figure(fig, f"period_ts_data_{i+1}.svg")
